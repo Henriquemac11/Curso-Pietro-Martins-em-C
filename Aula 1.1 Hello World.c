7fbdb12813529eb69a6da38cecc09f5c96443cec
Henrique Maciel <164898450+Henriquemac11@users.noreply.github.com>
2025-08-07 12:41:40 -0300

    Create Aula 1.1 Hello World.c

---
+++ Aula 1.1 Hello World.c
@@ -0,0 +1,10 @@
+#include <stdio.h> //Biblioteca de entrada e saida
+#include <stdlib.h> //Biblioteca com funcionalidades do sistema 
+
+int main(){ //Codigo
+
+	printf("Hello, World!\n"); //Operacao de saida de dados
+	
+	system("pause"); // Funcaoo para executar pelo windows, Pressione qualquer tecla para continuar
+	
+}
