d524f882f964780c1a3d3f1e3da6415864c93e8d
Henrique Maciel <164898450+Henriquemac11@users.noreply.github.com>
2025-08-07 12:44:28 -0300

    Create Aula 2.1 saida de dados.c

---
+++ Aula 2.1 saida de dados.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+printf("Oi, tudo bem? Tenho 16 anos e programo.\n"); //Sa�da de dados
+
+printf("Valor inteiro %d.\n", 10); //N�meros inteiros
+
+printf("valor real:%f.\n", 3.14159265 ); // float
+
+printf("valor real com apenas duas casas:%.2f.\n", 3.14159265); // float
+
+printf("Dado de texto %c.\n", 'a'); // �nica letra
+
+printf("Dado de texto: %s.\n", "Testando"); //string
+
+system("pause"); 
+}
